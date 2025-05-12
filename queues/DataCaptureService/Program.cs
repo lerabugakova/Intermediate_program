169c57ed73a664167829d735984a952ca35e8704
Valeriya Bugakova <valeriya.bugakova@wickes.co.uk>
2025-05-12 17:24:08 +0500

    Consumer

--- queues/DataCaptureService/Program.cs
+++ queues/DataCaptureService/Program.cs
@@ -5,27 +5,32 @@ namespace DataCaptureService
 {
     class Program
     {
+        const string filePath = "example.txt";
         static async Task Main(string[] args)
         {
-            string filePath = "example.txt";
-            string content = "This is a sample text file created by C#.";
+            var number = 1;
+            while (true)
+            {
+                string content = $"Document Number {number++} .";
 
-            File.WriteAllText(filePath, content);
+                File.WriteAllText(filePath, content);
 
-            string messageBody = File.ReadAllText(filePath);
+                string messageBody = File.ReadAllText(filePath);
 
-            var factory = new ConnectionFactory { HostName = "localhost" };
-            using var connection = await factory.CreateConnectionAsync();
-            using var channel = await connection.CreateChannelAsync();
+                var factory = new ConnectionFactory { HostName = "localhost" };
+                using var connection = await factory.CreateConnectionAsync();
+                using var channel = await connection.CreateChannelAsync();
 
-            await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+                await channel.QueueDeclareAsync(queue: "data", durable: false, exclusive: false, autoDelete: false,
+                                                 arguments: null);
 
-            var body = Encoding.UTF8.GetBytes(messageBody);
-            await channel.BasicPublishAsync(exchange: "logs", routingKey: string.Empty, body: body);
-            Console.WriteLine($" [x] Sent {messageBody}");
+                var body = Encoding.UTF8.GetBytes(messageBody);
+                await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "data", body: body);
+                Console.WriteLine($" [x] Sent {messageBody}");
 
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ queues/ImageTransformationServices/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "data", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+while (true)
+{
+    Console.WriteLine(" [*] Waiting for messages.");
+    var consumer = new AsyncEventingBasicConsumer(channel);
+    consumer.ReceivedAsync += (model, ea) =>
+    {
+        byte[] body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($" [x] {message}");
+        return Task.CompletedTask;
+    };
+
+    await channel.BasicConsumeAsync("data", autoAck: true, consumer: consumer);
+
+    Console.ReadLine();
+}
