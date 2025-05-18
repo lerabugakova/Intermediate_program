906d9f5f7e3f58c328b132944b98d3720f60480b
Valeriya Bugakova <valeriya.bugakova@wickes.co.uk>
2025-05-18 13:42:49 +0500

    first task for refactoring

---
+++ 04_Analyzing/FirstTask/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FirstTask;
+
+Console.WriteLine("Start!");
+var test = new ClassToRefactor();
+test.GeneratePasswordHashUsingSalt();
+Console.WriteLine("Done");
+Console.ReadLine();
+
