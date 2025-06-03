7c9b29cc1077f73599eaf94f6178c440be303235
Valeriya Bugakova <valeriya.bugakova@wickes.co.uk>
2025-06-03 17:52:24 +0500

    complete

--- 05_Linq/Expressions_and_IQueryable.Tasks.Week2/Expressions.Task3.E3SQueryProvider.Test/E3SAndOperatorSupportTests.cs
+++ 05_Linq/Expressions_and_IQueryable.Tasks.Week2/Expressions.Task3.E3SQueryProvider.Test/E3SAndOperatorSupportTests.cs
@@ -35,7 +35,8 @@ namespace Expressions.Task3.E3SQueryProvider.Test
              */
 
             // todo: create asserts for this test by yourself, because they will depend on your final implementation
-            throw new NotImplementedException("Please implement this test and the appropriate functionality");
+            string translated = translator.Translate(expression);
+            Assert.Equal("Manager:(John*), Workstation:(EPRUIZHW006)", translated);
         }
 
         #endregion
--- 05_Linq/Expressions_and_IQueryable.Tasks.Week2/Expressions.Task3.E3SQueryProvider/ExpressionToFTSRequestTranslator.cs
+++ 05_Linq/Expressions_and_IQueryable.Tasks.Week2/Expressions.Task3.E3SQueryProvider/ExpressionToFTSRequestTranslator.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Expressions.Task3.E3SQueryProvider
 {
@@ -23,6 +24,25 @@ namespace Expressions.Task3.E3SQueryProvider
 
         #region protected methods
 
+        protected override Expression VisitConditional(ConditionalExpression node)
+        {
+            switch (node.NodeType)
+            {
+                case ExpressionType.Equal: Visit(node);
+
+                    Visit(node);
+                    _resultStringBuilder.Append("(");
+                    Visit(node);
+                    _resultStringBuilder.Append(")");
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Operation '{node.NodeType}' is not supported");
+            };
+
+            return node;
+        }
+
         protected override Expression VisitMethodCall(MethodCallExpression node)
         {
             if (node.Method.DeclaringType == typeof(Queryable)
@@ -33,6 +53,51 @@ namespace Expressions.Task3.E3SQueryProvider
 
                 return node;
             }
+
+            if (node.Method.DeclaringType == typeof(String)
+                && node.Method.Name == "Equals")
+            {
+                Visit(node.Object);
+                _resultStringBuilder.Append("(");
+                Visit(node.Arguments[0]);
+                _resultStringBuilder.Append(")");
+
+                return node;
+            }
+
+            if (node.Method.DeclaringType == typeof(String)
+                && node.Method.Name == "StartsWith")
+            {
+                Visit(node.Object);
+                _resultStringBuilder.Append("(");
+                Visit(node.Arguments[0]);
+                _resultStringBuilder.Append("*)");
+
+                return node;
+            }
+
+            if (node.Method.DeclaringType == typeof(String)
+                && node.Method.Name == "EndsWith")
+            {
+                Visit(node.Object);
+                _resultStringBuilder.Append("(*");
+                Visit(node.Arguments[0]);
+                _resultStringBuilder.Append(")");
+
+                return node;
+            }
+
+            if (node.Method.DeclaringType == typeof(String)
+                && node.Method.Name == "Contains")
+            {
+                Visit(node.Object);
+                _resultStringBuilder.Append("(*");
+                Visit(node.Arguments[0]);
+                _resultStringBuilder.Append("*)");
+
+                return node;
+            }
+
             return base.VisitMethodCall(node);
         }
 
@@ -41,18 +106,37 @@ namespace Expressions.Task3.E3SQueryProvider
             switch (node.NodeType)
             {
                 case ExpressionType.Equal:
-                    if (node.Left.NodeType != ExpressionType.MemberAccess)
-                        throw new NotSupportedException($"Left operand should be property or field: {node.NodeType}");
-
-                    if (node.Right.NodeType != ExpressionType.Constant)
-                        throw new NotSupportedException($"Right operand should be constant: {node.NodeType}");
-
-                    Visit(node.Left);
-                    _resultStringBuilder.Append("(");
-                    Visit(node.Right);
-                    _resultStringBuilder.Append(")");
+                    if (node.Left.NodeType != ExpressionType.MemberAccess && node.Right.NodeType != ExpressionType.Constant)
+                    {
+                        Visit(node.Right);
+                        _resultStringBuilder.Append("(");
+                        Visit(node.Left);
+                        _resultStringBuilder.Append(")");
+                    }
+                    else
+                    {
+                        Visit(node.Left);
+                        _resultStringBuilder.Append("(");
+                        Visit(node.Right);
+                        _resultStringBuilder.Append(")");
+                    }
                     break;
-
+                    case ExpressionType.AndAlso:
+                        if (node.Left.NodeType != ExpressionType.MemberAccess && node.Right.NodeType != ExpressionType.Constant)
+                        {
+                            Visit(node.Right);
+                            _resultStringBuilder.Append(", ");
+                            Visit(node.Left);
+                            _resultStringBuilder.Append("");
+                        }
+                        else
+                        {
+                            Visit(node.Right);
+                            _resultStringBuilder.Append(", ");
+                            Visit(node.Left);
+                            _resultStringBuilder.Append("");
+                        }
+                    break;
                 default:
                     throw new NotSupportedException($"Operation '{node.NodeType}' is not supported");
             };
