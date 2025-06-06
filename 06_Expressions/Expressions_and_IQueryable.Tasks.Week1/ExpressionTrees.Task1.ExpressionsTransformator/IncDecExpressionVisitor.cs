4b74d72fa8ae42f1d06feb04c6e06c86dac0adc9
Valeriya Bugakova <valeriya.bugakova@wickes.co.uk>
2025-06-06 18:19:24 +0500

    Task 1

--- 06_Expressions/Expressions_and_IQueryable.Tasks.Week1/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
+++ 06_Expressions/Expressions_and_IQueryable.Tasks.Week1/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -1,9 +1,51 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
-namespace ExpressionTrees.Task1.ExpressionsTransformer
+public class ExpressionTransformer : ExpressionVisitor
 {
-    public class IncDecExpressionVisitor : ExpressionVisitor
+    private readonly Dictionary<ParameterExpression, ConstantExpression> _parameterReplacements;
+
+    public ExpressionTransformer(Dictionary<ParameterExpression, ConstantExpression> parameterReplacements)
     {
-        // todo: feel free to add your code here
+        _parameterReplacements = parameterReplacements;
+    }
+
+    protected override Expression VisitBinary(BinaryExpression node)
+    {
+        // Check for + 1 and - 1 transformations
+        if (node.NodeType == ExpressionType.Add && node.Right is ConstantExpression rightConstant && (int)rightConstant.Value == 1)
+        {
+            if (_parameterReplacements.TryGetValue((ParameterExpression)node.Left, out ConstantExpression replacement))
+            {
+                return Expression.Increment(replacement);
+            }
+        }
+        else if (node.NodeType == ExpressionType.Subtract && node.Right is ConstantExpression rightConstantSub && (int)rightConstantSub.Value == 1)
+        {
+            if (_parameterReplacements.TryGetValue((ParameterExpression)node.Left, out ConstantExpression replacement))
+            {
+                return Expression.Decrement(replacement);
+            }
+        }
+
+        return base.VisitBinary(node);
+    }
+
+    protected override Expression VisitParameter(ParameterExpression node)
+    {
+        // Replace parameters with constants if they exist in the replacements dictionary
+        if (_parameterReplacements.TryGetValue(node, out ConstantExpression replacement))
+        {
+            return replacement;
+        }
+
+        return base.VisitParameter(node);
+    }
+
+    public static Expression Transform(Expression sourceExpression, Dictionary<ParameterExpression, ConstantExpression> parameterReplacements)
+    {
+        var transformer = new ExpressionTransformer(parameterReplacements);
+        return transformer.Visit(sourceExpression);
     }
 }
--- 06_Expressions/Expressions_and_IQueryable.Tasks.Week1/ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
+++ 06_Expressions/Expressions_and_IQueryable.Tasks.Week1/ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
@@ -7,6 +7,8 @@
  * The results could be printed in console or checked via Debugger using any Visualizer.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
@@ -17,7 +19,32 @@ namespace ExpressionTrees.Task1.ExpressionsTransformer
             Console.WriteLine("Expression Visitor for increment/decrement.");
             Console.WriteLine();
 
-            // todo: feel free to add your code here
+            ParameterExpression paramA = Expression.Parameter(typeof(int), "a");
+            ParameterExpression paramB = Expression.Parameter(typeof(int), "b");
+
+            // Source expression: a + 1 - b
+            Expression sourceExpression = Expression.Subtract(
+                Expression.Add(paramA, Expression.Constant(1)),
+                paramB
+            );
+
+            // Parameter replacements: a -> 5, b -> 3
+            var parameterReplacements = new Dictionary<ParameterExpression, ConstantExpression>
+        {
+            { paramA, Expression.Constant(5) },
+            { paramB, Expression.Constant(3) }
+        };
+
+            // Transform the expression
+            Expression transformedExpression = ExpressionTransformer.Transform(sourceExpression, parameterReplacements);
+
+            // Output the resulting expression tree
+            Console.WriteLine(transformedExpression);
+
+            // Compile and execute the transformed expression
+            var lambda = Expression.Lambda<Func<int>>(transformedExpression);
+            var compiledLambda = lambda.Compile();
+            Console.WriteLine(compiledLambda());
 
             Console.ReadLine();
         }
